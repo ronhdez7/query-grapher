8106f484fe444bfefef4ff92d60286501418b4a1
ronald hernandez <ronaldhp2007@gmail.com>
2024-03-04 17:40:57 -0500

    improved parsing

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -26,21 +26,35 @@ export class Parser {
     return body;
   }
 
-  private parseBody(query: any, root: SchemaType[string] | DataValue): string {
-    if (!query) return "";
+  private parseBody(
+    query: any,
+    root: SchemaType[string] | DataValue
+  ): string | null {
+    // make sure query is valid
+    if (!query) return null;
     else if (typeof query === "string") return query;
 
     // Reference
     if (typeof root === "string") {
       const rootValue = this.schema[getName(root)];
-      if (!rootValue) return "";
-      return this.parseBody(query, rootValue);
+      if (rootValue === undefined) return null;
+      else if (rootValue === "") return "";
+      else return this.parseBody(query, rootValue);
     }
 
     // Union | List | FieldWithArgs
     else if (root instanceof Array) {
-      if (root.length === 1) this.parseBody(query, root[0]);
+      // List
+      if (root.length === 1) {
+        return this.parseBody(query, root[0]);
+      }
+
+      // FieldWithArgs
       else if (root[0] && typeof root[0] !== "string") {
+        // parse all fields from root[1] (data) if all arguments are optional
+        const rootData = root[1];
+        if (rootData === undefined) return null;
+
         if (query === true) {
           const args = root[0] as Arguments;
           for (const argKey in args) {
@@ -50,13 +64,13 @@ export class Parser {
               typeof argValue === "string" &&
               argValue.endsWith("!")
             )
-              return "null";
+              return null;
           }
-          return this.parseBody(true, root[1] ?? "");
+          return this.parseBody(true, rootData);
         }
 
         const { args, data } = query;
-        if (!data) return "";
+        if (!data) return null;
 
         let output = "";
 
@@ -77,45 +91,68 @@ export class Parser {
           if (argsSection !== "") output += `(${argsSection.slice(0, -1)}) `;
         }
 
-        output += this.parseBody(query["data"], root[1] ?? "");
+        const body = this.parseBody(query["data"], rootData);
+        if (body === null) return null;
+
+        output += body;
         return output;
       }
-      return "";
+      // Union
+      else {
+        return null;
+      }
+      return null;
     }
 
     // Object with fields
     else {
+      // check if query is a fragment
       if (query instanceof Fragment) {
         return this.parseBody(query.getFragment(), root);
-      } else if (query === true) {
+      }
+
+      // check if all subfields should be parsed
+      else if (query === true) {
         let output = "{\n";
 
         for (const key in root) {
           const value = root[key];
           if (!value) continue;
-          // else if (typeof value === "string") {
-          // 	output += key + " " + this.parseBody(true, value) + "\n"
-          // } else if (value instanceof Array) {
-          // 	output += key + " " + this.parseBody(true, value) + "\n"
-          // }
-          output += key + " " + this.parseBody(true, value) + "\n";
+
+          const body = this.parseBody(true, value);
+          if (body === null) continue;
+          output += "" + key + " " + body + "\n";
         }
 
         return output + "}";
-      } else {
+      }
+
+      // check if query has invalid type
+      else if (
+        typeof query !== "object" ||
+        query instanceof Array ||
+        Object.keys(query).length === 0
+      ) {
+        return null;
+      }
+
+      // parse fields of query
+      else {
         let output = "{\n";
         for (const key in query) {
           const newRoot = root[key];
           if (!query[key] || !newRoot) continue;
 
-          output += key + " " + this.parseBody(query[key], newRoot) + "\n";
+          const body = this.parseBody(query[key], newRoot);
+          if (body === null) continue;
+          output += "" + key + " " + body + "\n";
         }
 
         return output + "}";
       }
     }
 
-    return "";
+    return null;
   }
 }
 
